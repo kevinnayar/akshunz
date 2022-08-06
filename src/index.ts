75a23cb878939b61e094a7ba236e263b8e792945
Kevin Nayar <kevin.nayar@gmail.com>
2022-08-05 22:42:37 -0500

    add testt

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,22 @@ export function capitalize(text: string) {
   return newWords.join(' ')
 }
 
+export function replaceInText(textIn: string, replaceTuples: Array<[string, string]>): string {
+  const textOut = replaceTuples.reduce((text, [key, value]) => {
+    text = text.replace(key, value)
+    return text
+  }, textIn)
+  return textOut
+}
+
 function main() {
-  const text = 'this is the main function'
-  const capitalized = capitalize(text)
+  const textIn = 'this is the secondary method'
+  const replaceTuples: Array<[string, string]> = [
+    ['secondary', 'primary'],
+    ['method', 'function'],
+  ]
+  const textOut = replaceInText(textIn, replaceTuples)
+  const capitalized = capitalize(textOut)
   return capitalized
 }
 
